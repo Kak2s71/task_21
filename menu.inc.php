05d7eb3fd75f017fbd7547fd9f5c486c5cc93817
Kak2s71 <87012123+Kak2s71@users.noreply.github.com>
2022-08-28 15:37:36 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,8 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Обо мне </a></li>
+                    <li><a href="#"> Знания </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
